04348ab5c0c4d0e9a341859a093282f4c93dbf28
rajatk-31 <rajat.yadav@zenways.io>
2019-01-12 11:50:43 +0530

    ok

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express'),
-app = express(),
-mongoose = require('mongoose'),
-bodyParser = require('body-parser'),
-config = require('./config/config.json')
+    app = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    config = require('./config/config.json')
 contacts = require('./routes/routes')
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
-mongoose.connect(config.MONGO, (err, data)=>{
-    if(err) throw err;
+mongoose.connect('mongodb://test:plmnko09876@ds121726.mlab.com:21726/school', (err, data) => {
+    if (err) throw err;
     else console.log("Database connected.")
 })
 app.use(function(req, res, next) {
@@ -19,8 +19,13 @@ app.use(function(req, res, next) {
     next()
 })
 app.set('secret', config.SECRET);
-app.use('/',contacts)
+app.use('/', contacts)
 
-app.listen(config.PORT, ()=>{
-    console.log("Server started at port : "+config.PORT);
+app.use(express.static(__dirname + '/dist'))
+app.use(function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
+
+app.listen(config.PORT, () => {
+    console.log("Server started at port : " + config.PORT);
 })
